61861eb6c02b35f9f8335b26908b5becfe7b0b91
sjy <stseo012@gmail.com>
2022-07-05 02:02:30 +0900

    feat: input value 받아오는 함수와 버튼에 이벤트 붙여주는 함수

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+import { CAR_NAME_MAX_LENGTH, FORWARD_MOVEMENT_THRESHOLD } from "./constants";
+import { Car } from "./car";
+export const getInputValue = (inputId: string) => {
+  const input = document.getElementById(inputId) as HTMLInputElement;
+  return input.value;
+};
+
+export const btnAddEventListener = (btnId: string, callBack: () => void) => {
+  const btn = document.getElementById(btnId);
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    callBack();
+  });
+};
