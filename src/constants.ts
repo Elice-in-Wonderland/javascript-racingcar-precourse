8a09293132e3a55c20f8eb6b3d59223ec73b0381
sjy <stseo012@gmail.com>
2022-07-05 01:58:53 +0900

    feat: 상수

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const CAR_NAME_MAX_LENGTH = 5;
+export const FORWARD_MOVEMENT_THRESHOLD = 4;
+
+export const INPUT_WRONG_MESSAGE = {
+  inputOverMaxLength: `자동차 이름을 ${CAR_NAME_MAX_LENGTH}자 이하로 입력해주세요`,
+};
